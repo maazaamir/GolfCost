c66d3075dff5446baa4f5eddd2104ccf4673a4e8
Maaz Aamir <you@example.com>
2026-03-08 03:48:06 +0530

    Updated homepage with golf hero banner and sections

--- script.js
+++ script.js
@@ -6,3 +6,33 @@
 //Hositing
 console.log("a")
 var a=12;
+document.querySelector("#arrow").addEventListener("click", function(){
+  document.querySelector("#page2").scrollIntoView({
+    behavior: "smooth"
+  });
+});
+const counters = document.querySelectorAll('.counter');
+
+counters.forEach(counter => {
+
+  counter.innerText = '0';
+
+  const updateCounter = () => {
+
+    const target = +counter.getAttribute('data-target');
+    const c = +counter.innerText;
+
+    const increment = target / 200;
+
+    if(c < target){
+      counter.innerText = `${Math.ceil(c + increment)}`;
+      setTimeout(updateCounter,10);
+    } else {
+      counter.innerText = target;
+    }
+
+  };
+
+  updateCounter();
+
+});
